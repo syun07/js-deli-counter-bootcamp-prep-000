9b4b50e6cd6f847b892af99d905794e4cbe565c5
Sarah Yun <sarahyun7@gmail.com>
2018-11-22 07:50:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeli) {
   if (katzDeli.length > 0) {
     var string = `The line is currently: 1. ${katzDeli[0]}, `
     for (let i = 1; i < katzDeli.length; i++) {
-      return string + `${i + 1}. ${katzDeli[i]}, `
+      string + `${i + 1}. ${katzDeli[i]}, `
     }
   } else if (katzDeli.length === 0) {
     return 'The line is currently empty.'
