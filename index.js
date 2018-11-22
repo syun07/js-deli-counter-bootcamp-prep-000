74c6094c397d243bf7676fb6b798945b29feb472
Sarah Yun <sarahyun7@gmail.com>
2018-11-22 08:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,6 @@ function nowServing(katzDeli) {
 }
 
 
-// function currentLine(katzDeli) {
-//   if (katzDeli.length > 0) {
-//     var string = `The line is currently: 1. ${katzDeli[0]}`;
-//     for (let i = 1; i < katzDeli.length; i++) {
-//       string += `, ${i + 1}. ${katzDeli[i]}`;
-//     }
-//     return string;
-//   } else if (katzDeli.length === 0) {
-//     return 'The line is currently empty.';
-//   }
-// }
-
 function currentLine(katzDeli) {
   if (katzDeli.length > 0) {
     var string = `The line is currently: 1. ${katzDeli[0]}`;
@@ -37,48 +25,3 @@ function currentLine(katzDeli) {
     return 'The line is currently empty.';
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var katzDeliLine = [];
-
-// function takeANumber(currentLine, name) {
-//   katzDeliLine.push(name);
-//   for (var i = 1; i < currentLine.length; i++) {
-//     console.log(`Welcome, ${name}. You are number ${i} in line.`)
-//   }
-
-
-
-
-//
-// function nowServing(katzDeli) {
-//   if (katzDeli.length > 0)
-//   return `Currently serving ${katzDeli[0]}.`
-//   katzDeli.shift;
-//
-//   if (katzDeli.length == 0)
-//   return 'There is nobody waiting to be served!'
-// }
